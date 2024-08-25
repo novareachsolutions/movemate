526e18386f6e7448f8bdac623a5551ff7ece482e
abdul-ahad12 <abdulkouzer@gmail.com>
2024-08-26 05:26:30 +0530

    feat- added a basic logging middleware

--- apps/backend/api-gateway/src/app.module.ts
+++ apps/backend/api-gateway/src/app.module.ts
@@ -1,10 +1,15 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Module, MiddlewareConsumer } from '@nestjs/common';
+import { LoggingMiddleware } from './middlewares/logging.middleware';
 
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
-export class AppModule {}
+export class AppModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(LoggingMiddleware)
+      .forRoutes('*');
+  }
+}
