ccfca9bad8c6a4fd904bef8acda8c272bc77967c
abdul-ahad12 <abdulkouzer@gmail.com>
2024-09-01 10:29:20 +0530

    refactor: removed unnecessary data in readme

--- apps/backend/core/src/app.module.ts
+++ apps/backend/core/src/app.module.ts
@@ -1,9 +1,28 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { OrderModule } from './order/order.module';
+import { CustomerModule } from './user/customer/customer.module';
+import { AdminModule } from './user/admin/admin.module';
+import { RiderModule } from './user/rider/rider.module';
+import { RestaurantOwnerModule } from './user/restaurantowner/restaurantowner.module';
+import { ShopOwnerModule } from './user/shopowner/shopowner.module';
+import { OrderItemModule } from './order/orderitem/orderitem.module';
+import { OrderFieldModule } from './order/orderfield/orderfield.module';
 
 @Module({
-  imports: [],
+  imports: [
+    UserModule,
+    OrderModule,
+    CustomerModule,
+    AdminModule,
+    RiderModule,
+    RestaurantOwnerModule,
+    ShopOwnerModule,
+    OrderItemModule,
+    OrderFieldModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
