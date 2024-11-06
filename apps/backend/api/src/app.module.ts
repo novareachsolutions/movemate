5cded29d93b3d5db12fc0ea514a4cd5243e4551d
theshdb <shdb0011@gmail.com>
2024-11-06 22:46:48 +0530

    feat: add postgres and migration setup

--- apps/backend/api/src/app.module.ts
+++ apps/backend/api/src/app.module.ts
@@ -4,12 +4,14 @@ import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { RedisModule } from './modules/redis/redis.module';
 import { AuthModule } from './modules/auth/auth.module';
+import { DatabaseModule } from './config/database/database.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
     }),
+    DatabaseModule,
     RedisModule,
     AuthModule,
   ],
---
+++ apps/backend/api/src/config/database/database.module.ts
@@ -0,0 +1,18 @@
+import { Module, OnModuleInit } from '@nestjs/common';
+import { DatabaseService } from './database.service';
+
+@Module({
+  providers: [DatabaseService],
+  exports: [DatabaseService],
+})
+export class DatabaseModule implements OnModuleInit {
+  // Initialize DatabaseService when the module is initialized
+  async onModuleInit() {
+    await DatabaseService.initialize();
+  }
+
+  // Ensure that DatabaseService is cleaned up when the module is destroyed
+  async onModuleDestroy() {
+    await DatabaseService.stop();
+  }
+}
