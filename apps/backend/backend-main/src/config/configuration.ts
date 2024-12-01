7455c602e14c3e986dff3620a247fc765ff30cde
theshdb <shdb0011@gmail.com>
2024-12-02 00:26:46 +0530

    refactor(backend): use config for env variables in constanst file

--- apps/backend/backend-main/src/config/configuration.ts
+++ apps/backend/backend-main/src/config/configuration.ts
@@ -7,6 +7,7 @@ export default (): Record<string, any> => ({
   database: {
     type: process.env.TYPEORM_CONNECTION,
     host: process.env.TYPEORM_HOST,
+    read_port: parseInt(process.env.TYPEORM_READ_PORT, 10),
     port: parseInt(process.env.TYPEORM_PORT, 10) || 5432,
     username: process.env.TYPEORM_USERNAME,
     password: process.env.TYPEORM_PASSWORD,
--- apps/backend/backend-main/src/constants.ts
+++ apps/backend/backend-main/src/constants.ts
@@ -1,11 +1,14 @@
+import configuration from "./config/configuration";
 import { logger } from "./logger";
 
+const config = configuration();
+
 export const DB_READ_NAME = "read";
-export let DB_READ_PORT = Number(process.env.DB_READ_PORT);
+export let DB_READ_PORT = config.database.read_port;
 if (!DB_READ_PORT) {
   logger.warn("DB_READ_PORT is not configured. Read replica will not be used.");
-  DB_READ_PORT = Number(process.env.TYPEORM_PORT);
+  DB_READ_PORT = config.database.port;
 }
 export const MAX_QUERY_EXECUTION_TIME = 7000;
 export const CONNECTION_TIMEOUT_MS = 60000;
-export const DB_CONNECTION_POOL_MAX = process.env.DB_CONNECTION_POOL_MAX || 10;
+export const DB_CONNECTION_POOL_MAX = config.database.poolMax || 10;
