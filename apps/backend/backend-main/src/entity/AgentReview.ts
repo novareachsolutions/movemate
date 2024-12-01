eb3dc95ac3651a370112ce3cfd4c50b50b3d61d2
theshdb <shdb0011@gmail.com>
2024-12-01 18:50:57 +0530

    refactor(root): move main backend folder to sub folder in backend

---
+++ apps/backend/backend-main/src/entity/AgentReview.ts
@@ -0,0 +1,44 @@
+import { Column, Entity, Index, ManyToOne, RelationId } from "typeorm";
+
+import { Agent } from "./Agent";
+import { BaseEntity } from "./BaseEntity";
+import { User } from "./User";
+
+@Index("IDX_agent_review_customerId", ["customerId"], {
+  where: '"deletedAt" IS NULL',
+})
+@Index("IDX_agent_review_agentId", ["agentId"], {
+  where: '"deletedAt" IS NULL',
+})
+@Entity()
+export class AgentReview extends BaseEntity {
+  @Column({ type: "float", nullable: false })
+  rating: number;
+
+  @Column({ type: "varchar", nullable: true })
+  comment: string;
+
+  @ManyToOne(() => User, {
+    cascade: true,
+    deferrable: "INITIALLY IMMEDIATE",
+    onDelete: "CASCADE",
+    nullable: false,
+  })
+  customer: User;
+
+  @RelationId((review: AgentReview) => review.customer)
+  @Column({ type: "integer" })
+  customerId: number;
+
+  @ManyToOne(() => Agent, {
+    cascade: true,
+    deferrable: "INITIALLY IMMEDIATE",
+    onDelete: "CASCADE",
+    nullable: false,
+  })
+  agent: Agent;
+
+  @RelationId((review: AgentReview) => review.agent)
+  @Column({ type: "integer" })
+  agentId: number;
+}
---
+++ apps/backend/backend-main/src/entity/PickupLocation.ts
@@ -0,0 +1,29 @@
+import { Column, Entity, OneToOne, RelationId } from "typeorm";
+
+import { BaseEntity } from "./BaseEntity";
+import { Order } from "./Order";
+
+@Entity()
+export class PickupLocation extends BaseEntity {
+  @Column({ type: "varchar", length: 255, nullable: false })
+  addressLine1: string;
+
+  @Column({ type: "varchar", length: 255, nullable: true })
+  addressLine2: string;
+
+  @Column({ type: "varchar", length: 255, nullable: true })
+  landmark: string;
+
+  @Column({ type: "float" })
+  latitude: number;
+
+  @Column({ type: "float" })
+  longitude: number;
+
+  @OneToOne(() => Order, (order) => order.pickupLocation)
+  order: Order;
+
+  @RelationId((pickupLocation: PickupLocation) => pickupLocation.order)
+  @Column({ type: "integer" })
+  orderId: number;
+}
