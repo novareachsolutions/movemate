18b4a5da221a82375032eec7f444822219ad8f2c
theshdb <shdb0011@gmail.com>
2024-12-09 00:29:16 +0530

    feat(backend): add basic websocket code for the project

---
+++ apps/backend/backend-main/src/shared/gateways/customer.notification.gateway.ts
@@ -0,0 +1,70 @@
+import {
+  ConnectedSocket,
+  MessageBody,
+  SubscribeMessage,
+  WebSocketGateway,
+} from "@nestjs/websockets";
+import { Socket } from "socket.io";
+
+import configuration from "../../config/configuration";
+import { BaseSocketGateway } from "./base.socket";
+
+const config = configuration();
+
+@WebSocketGateway({
+  port: config.websocketPorts.customerNotification,
+  cors: { origin: config.corsOrigin },
+})
+export class CustomerNotificationGateway extends BaseSocketGateway {
+  @SubscribeMessage("agentAcceptedRequest")
+  handleAgentAcceptedRequest(
+    @MessageBody() data: any,
+    @ConnectedSocket() _client: Socket
+  ): void {
+    this.sendMessageToClient(data.customerId, "agentAcceptedRequest", data);
+  }
+
+  @SubscribeMessage("agentArrived")
+  handleAgentArrived(
+    @MessageBody() data: any,
+    @ConnectedSocket() _client: Socket
+  ): void {
+    this.sendMessageToClient(data.customerId, "agentArrived", data);
+  }
+
+  @SubscribeMessage("requestCompleted")
+  handleRequestCompleted(
+    @MessageBody() data: any,
+    @ConnectedSocket() _client: Socket
+  ): void {
+    this.sendMessageToClient(data.customerId, "requestCompleted", data);
+  }
+
+  @SubscribeMessage("agentExpectedArrivalTime")
+  handleAgentExpectedArrivalTime(
+    @MessageBody() data: any,
+    @ConnectedSocket() _client: Socket
+  ): void {
+    this.sendMessageToClient(data.customerId, "agentExpectedArrivalTime", data);
+  }
+
+  @SubscribeMessage("agentExpectedRequestCompletionTime")
+  handleAgentExpectedCompletionTime(
+    @MessageBody() data: any,
+    @ConnectedSocket() _client: Socket
+  ): void {
+    this.sendMessageToClient(
+      data.customerId,
+      "agentExpectedCompletionTime",
+      data
+    );
+  }
+
+  @SubscribeMessage("agentStatus")
+  handleAgentStatus(
+    @MessageBody() data: any,
+    @ConnectedSocket() _client: Socket
+  ): void {
+    this.sendMessageToClient(data.customerId, "agentStatus", data);
+  }
+}
