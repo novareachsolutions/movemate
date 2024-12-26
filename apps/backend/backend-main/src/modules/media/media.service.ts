8d44eae4c58ec2c2950eb919a57bcd6a16892c94
abdul-ahad12 <abdulkouzer@gmail.com>
2024-12-26 09:08:04 +0000

    feat: edia module error handling

--- apps/backend/backend-main/src/modules/media/media.service.ts
+++ apps/backend/backend-main/src/modules/media/media.service.ts
@@ -1,10 +1,10 @@
-// src/media/media.service.ts
-
-import { Injectable, InternalServerErrorException, Inject } from '@nestjs/common';
+import { Injectable, Inject } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import * as AWS from 'aws-sdk';
 import { ManagedUpload } from 'aws-sdk/clients/s3';
 import { logger } from '../../logger';
+import { MediaDeleteError, MediaInvalidKeyError, MediaMissingFileError, MediaUploadError } from '../../shared/errors/media';
+
 
 @Injectable()
 export class MediaService {
@@ -16,17 +16,20 @@ export class MediaService {
   ) {
     this.bucketName = this.configService.get<string>('AWS_S3_BUCKET_NAME');
     if (!this.bucketName) {
-      throw new InternalServerErrorException('S3 Bucket name is not configured');
+      throw new MediaInvalidKeyError('S3 Bucket name is not configured');
     }
   }
 
   async uploadFile(file: Express.Multer.File): Promise<string> {
+    if (!file) {
+      throw new MediaMissingFileError("No file was provided in the request.");
+    }
     const params: AWS.S3.PutObjectRequest = {
       Bucket: this.bucketName,
       Key: `${Date.now()}-${file.originalname}`,
       Body: file.buffer,
       ContentType: file.mimetype,
-      ACL: 'public-read', 
+      ACL: 'public-read',
     };
 
     try {
@@ -35,11 +38,15 @@ export class MediaService {
       return uploadResult.Location;
     } catch (error) {
       logger.error('Error uploading file to S3', error);
-      throw new InternalServerErrorException('Failed to upload file');
+      throw new MediaUploadError('Failed to upload file');
     }
   }
 
   async deleteFile(key: string): Promise<string> {
+    if (!key) {
+      throw new MediaInvalidKeyError('File key is required for deletion');
+    }
+
     const params: AWS.S3.DeleteObjectRequest = {
       Bucket: this.bucketName,
       Key: key,
@@ -51,7 +58,7 @@ export class MediaService {
       return `File with key ${key} deleted successfully.`;
     } catch (error) {
       logger.error(`Error deleting file with key ${key} from S3`, error);
-      throw new InternalServerErrorException('Failed to delete file');
+      throw new MediaDeleteError(`Failed to delete file with key ${key}`);
     }
   }
 }
