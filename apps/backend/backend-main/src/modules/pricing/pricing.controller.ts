8dc14d9ad367087b3e52f81eb811a32071ddf7e5
abdul-ahad12 <abdulkouzer@gmail.com>
2025-01-02 21:46:15 +0000

    add guard in controller

--- apps/backend/backend-main/src/modules/pricing/pricing.controller.ts
+++ apps/backend/backend-main/src/modules/pricing/pricing.controller.ts
@@ -1,15 +1,19 @@
-// src/pricing/pricing.controller.ts
-
-import { Controller, Post, Body } from '@nestjs/common';
+import { Controller, Post, Body, UseGuards } from '@nestjs/common';
 import { PricingService } from './pricing.service';
 import { TFareCalculation } from './pricing.type';
 import { IApiResponse } from '../../shared/interface';
+import { AuthGuard } from '../../shared/guards/auth.guard';
+import { RoleGuard } from '../../shared/guards/roles.guard';
+import { Roles } from '../../shared/decorators/roles.decorator';
+import { UserRoleEnum } from '../../shared/enums';
 
 @Controller('pricing')
+@UseGuards(AuthGuard, RoleGuard)
 export class PricingController {
     constructor(private readonly pricingService: PricingService) { }
 
     @Post('calculate')
+    @Roles(UserRoleEnum.CUSTOMER, UserRoleEnum.AGENT, UserRoleEnum.ADMIN)
     calculateFare(@Body() body: TFareCalculation): IApiResponse<string> {
         const fare = this.pricingService.calculateFare(body);
         return {
