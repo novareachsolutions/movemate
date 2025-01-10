35bbb40829cbda9b28a1ea07a568cec3642f3090
abdul-ahad12 <abdulkouzer@gmail.com>
2025-01-10 00:31:18 +0000

    feat(@movemate/backend): add controoler in referral module

--- apps/backend/backend-main/src/modules/referral/referral.module.ts
+++ apps/backend/backend-main/src/modules/referral/referral.module.ts
@@ -1,9 +1,11 @@
 import { Module } from "@nestjs/common";
 
+import { ReferralController } from "./referral.controller";
 import { ReferralService } from "./referral.service";
 
 @Module({
   imports: [],
+  controllers: [ReferralController],
   providers: [ReferralService],
   exports: [ReferralService],
 })
